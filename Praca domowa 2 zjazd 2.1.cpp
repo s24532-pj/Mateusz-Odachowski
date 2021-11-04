c613d65de0a2d58ebd29f3b720ac05db974d1ac1
s24532-pj <92215115+s24532-pj@users.noreply.github.com>
2021-11-04 19:57:58 +0100

    Add files via upload

---
+++ Praca domowa 2 zjazd 2.1.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    cout << "Podaj n: ";
+    unsigned int n;
+    cin >> n;
+    string res = "Suma szeregu: ";
+    unsigned int res_i = 0;
+    for (int i = 0; i < n; ++i) {
+        unsigned int sum = 0;
+        for (int j = 0; j < i; ++j) {
+            cout << j + 1 << " + ";
+            sum += j + 1;
+        }
+        cout << i + 1 << " = " << sum + i + 1 << endl;
+        res_i += sum + i + 1;
+        if (i < n - 1) {
+            res += to_string(sum + i + 1);
+            res += " + ";
+        }
+        else {
+            res += to_string(sum + i + 1);
+        }
+    }
+    res += " = ";
+    res += to_string(res_i);
+    cout << res << endl;
+
+    return 0;
+}
