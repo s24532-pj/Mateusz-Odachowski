f99e958d5f2da6e99e23119a60552f4a856f2b5b
s24532-pj <92215115+s24532-pj@users.noreply.github.com>
2021-11-19 17:28:51 +0100

    Add files via upload

---
+++ Zadanieee 3.1.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+    float x, y;
+
+    cout << "Podaj x i y: ";
+    cin >> x >> y;
+
+    cout << "Suma: " << fixed << setprecision(2) << x + y << endl;
+    cout << "Roznica: " << fixed << setprecision(2) << x - y << endl;
+    cout << "Iloczyn: " << fixed << setprecision(2) << x * y << endl;
+    cout << "Iloraz: " << fixed << setprecision(2) << x / y << endl;
+
+
+    return 0;
+
+}
+
